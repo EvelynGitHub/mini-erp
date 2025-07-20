dff9dbe18aeea9fa3100edc6192e2e67e4ce5b9e
Evelyn <evelynbrandao15@gmail.com>
2025-07-20 19:15:19 -0300

    feat: Ajuste na página de variações

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Controllers\EstoqueController;
+use App\Controllers\VariacaoController;
+
 session_start();
 
 // Carrega o autoloader do Composer
@@ -19,11 +22,23 @@ JobRunner::setPDO($pdo);
 
 $page = $_GET['page'] ?? 'produtos';
 
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method != 'GET') {
+    header('Content-Type: application/json');
+}
+
+$bodyHttpInput = json_decode(file_get_contents('php://input'), true);
+
 switch ($page) {
     case 'produtos':
         $controller = new ProdutoController($pdo);
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $controller->store();
+        if ($method === 'POST') {
+            $controller->store($bodyHttpInput);
+        } elseif ($method === 'PUT') {
+            $controller->update($bodyHttpInput);
+        } elseif ($method === 'DELETE') {
+            $controller->delete($bodyHttpInput);
         } else {
             $controller->index();
         }
@@ -32,10 +47,10 @@ switch ($page) {
     case 'carrinho':
         $controller = new PedidoController($pdo);
         $action = $_GET['action'] ?? null;
-        if ($action === 'add' && isset($_GET['id'])) {
-            $controller->add($_GET['id'], 1);
-        } else if ($action === 'remove' && isset($_GET['id'])) {
-            $controller->remove($_GET['id']);
+        if ($action === 'add' && isset($_GET['id']) && isset($_GET['variacao'])) {
+            $controller->add($_GET['id'], $_GET['variacao'], 1);
+        } else if ($action === 'remove' && isset($_GET['id']) && isset($_GET['variacao'])) {
+            $controller->remove($_GET['id'], $_GET['variacao']);
         } else if ($action === 'checkout') {
             $controller->checkout();
         } else {
@@ -51,6 +66,43 @@ switch ($page) {
     case 'webhook':
         $controller = new WebhookController($pdo);
         $controller->handle();
+        break;
+
+    case 'variacao':
+        $controller = new VariacaoController($pdo);
+        $type = $bodyHttpInput['type'] ?? null;
+        if ($method === 'POST') {
+            if ($type === 'variacao') {
+                $controller->storeVariacao();
+            } elseif ($type === 'grupo') {
+                $controller->storeGrupoVariacao();
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Tipo de requisição inválido.']);
+            }
+        } elseif ($method === 'PUT') {
+            if ($type === 'variacao') {
+                $controller->updateVariacao();
+            } elseif ($type === 'grupo') {
+                $controller->updateGrupoVariacao();
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Tipo de requisição inválido.']);
+            }
+        } elseif ($method === 'DELETE') {
+            if ($type === 'variacao') {
+                $controller->deleteVariacao();
+            } elseif ($type === 'grupo') {
+                $controller->deleteGrupoVariacao();
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Tipo de requisição inválido.']);
+            }
+        } else {
+            $controller->index();
+        }
+        break;
+
+    case 'estoque':
+        $controller = new EstoqueController($pdo);
+        $controller->create($bodyHttpInput);
         break;
 
     default:
